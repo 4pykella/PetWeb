6f27a091c62f5c0f7873e284b61e2f03c50feb4b
4pykella <55879219+4pykella@users.noreply.github.com>
2026-07-28 04:06:50 +0300

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -6,5 +6,5 @@ docker run -it --rm \
   -v $(pwd)/data/certbot/www:/var/www/certbot \
   -p 80:80 \
   certbot/certbot certonly --standalone \
-  -d www.chepykella.ru -d prometheus.chepykella.ru -d prometheus.chepykella.ru -d grafana.chepykella.ru \
+  -d www.chepykella.ru -d prometheus.chepykella.ru -d prometheus.chepykella.ru -d grafana.chepykella.ru -d home.chepykella.ru \
   --email DanilMkrtchian@gmail.com --agree-tos --no-eff-email --force-renew
